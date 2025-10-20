34b1d225127bc62fdb35dfed67cb25df1be58943
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2025-10-21 04:26:07 +0900

    test 페이지 작업

--- script/diagnosis.js
+++ script/diagnosis.js
@@ -28,3 +28,51 @@ window.addEventListener('DOMContentLoaded', () => {
         daySelect.appendChild(option);
     }
 });
+
+
+// q1에서 card 선택했을 때 다음 질문 넘어감
+document.addEventListener('DOMContentLoaded', () => {
+    const selectCards = document.querySelectorAll('.select_card');
+
+    selectCards.forEach(card => {
+        card.addEventListener('click', () => {
+        const questionArea = document.querySelector('.question_area');
+        questionArea.style.top = '-100vh'; // 위로 이동
+        });
+    });
+});
+
+// q2에서 card 선택했을 때 다음 질문 넘어감
+document.addEventListener('DOMContentLoaded', () => {
+    const selectCards = document.querySelectorAll('.select_circle');
+
+    selectCards.forEach(card => {
+        card.addEventListener('click', () => {
+        const questionArea = document.querySelector('.question_area');
+        questionArea.style.top = '-200vh'; // 위로 이동
+        });
+    });
+});
+
+// q3에서 card 선택했을 때 다음 질문 넘어감
+document.addEventListener('DOMContentLoaded', () => {
+    const nextBtn = document.querySelector('.next_btn_wrap');
+
+    nextBtn.addEventListener('click', () => {
+        const questionArea = document.querySelector('.question_area');
+        questionArea.style.top = '-300vh'; // 위로 이동
+    });
+});
+
+// slider 막대기 활성화
+const slider = document.querySelector('#slider');
+
+function updateSliderBackground(slider) {
+    const val = (slider.value - slider.min) / (slider.max - slider.min) * 100;
+    slider.style.background = `linear-gradient(to right, #5CBFFF ${val}%, #F5F7FA ${val}%)`;
+}
+
+slider.addEventListener('input', () => updateSliderBackground(slider));
+
+// 초기 배경도 설정해줘야 함!
+updateSliderBackground(slider);
